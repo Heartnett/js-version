47938d7b867ae17e136a7d362ac11bd4993dd47a
Heartnett <Heartnett@users.noreply.github.com>
2016-11-25 07:02:47 +0200

    Create js-version.js

---
+++ js-version.js
@@ -0,0 +1,37 @@
+(function() {
+    "use strict";
+
+    var versions = (function() {
+        var array = [];
+        var major = 1;
+        var minor = 1;
+        while(major < 3) {
+            while(minor < 10) {
+                array.push(major + "." + minor++);
+            }
+            major++;
+            minor = 0;
+        }
+        return array.reverse();
+    })()
+
+    window.jsv = "";
+
+    var script = undefined;
+    var version = undefined;
+    var scripts = [];
+    while(versions.length !== 0) {
+        version = versions.pop();
+        scripts.push((script = document.createElement("script")));
+        script.setAttribute("language", "javascript" + version);
+        script.text = "window.jsv = '" + version + "';";
+        document.head.appendChild(script);
+    }
+
+    while(scripts.length !== 0) {
+        scripts.pop().remove();
+    }
+
+    script = scripts = version = versions = undefined;
+
+})();
